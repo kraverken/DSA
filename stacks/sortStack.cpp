eb3a1513660f4bb22147f24a0edf4fc7d84d85ff
Aryan <aryanverma170372@gmail.com>
2022-08-26 23:38:13 +0530

    sorting a stack

---
+++ stacks/sortStack.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+void sortedInsert(stack<int> &stack, int num)
+{
+    if (stack.empty() || (!stack.empty() && stack.top() < num))
+    {
+        stack.push(num);
+        return;
+    }
+    int n = stack.top();
+    stack.pop();
+    sortedInsert(stack, num);
+    stack.push(n);
+}
+void sortStack(stack<int> &stack)
+{
+    // Write your code here
+    if (stack.empty())
+    {
+        return;
+    }
+    int num = stack.top();
+    stack.pop();
+    sortStack(stack);
+    sortedInsert(stack, num);
+}
+
+int main()
+{
+    stack<int> s;
+    s.push(-3);
+    s.push(14);
+    s.push(18);
+    s.push(-5);
+    s.push(30);
+    sortStack(s);
+    cout << "Top element->" << s.top() << endl;
+    s.pop();
+    cout << "Top element->" << s.top() << endl;
+    s.pop();
+    cout << "Top element->" << s.top() << endl;
+    s.pop();
+    cout << "Top element->" << s.top() << endl;
+    s.pop();
+    cout << "Top element->" << s.top() << endl;
+    s.pop();
+    return 0;
+}
